914d47eeb362c4039f0867ffb2cd397445726b16
&! Tekky#1337 <98614666+xtekky@users.noreply.github.com>
2022-08-22 02:43:54 +0200

    Update main.py

--- main.py
+++ main.py
@@ -45,18 +45,3 @@ class TempMail:
             timeout = self.timeout
         ).json()
         return response["bodyHtml"]
-
-email_client = TempMail()
-token, email = email_client.get_mail()
-print(email)
-print(token)
-
-while True:
-    time.sleep(5)
-    messages = email_client.fetch_inbox()
-    print(messages)
-    
-    if len(messages["messages"]) > 0:
-
-        email_content = email_client.get_message_content(messages["messages"][0]["_id"])
-        print(email_content)
