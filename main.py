1c04a05d223fbc487be450927842daafed244823
xtekky <fkahdias@gmail.com>
2022-08-22 02:43:01 +0200

    initial upload

---
+++ main.py
@@ -0,0 +1,62 @@
+import html
+import client 
+import json
+import time
+
+class TempMail:
+    def __init__(self, proxies: str = None, timeout: int = 15, bearer_token: str or None = None) -> None:
+        self.session = client.init()
+        self.base_url = 'https://web2.temp-mail.org'
+        self.proxies = proxies
+        self.timeout = timeout
+        
+        self.session.headers['authorization'] = f'Bearer {bearer_token}' if bearer_token else None
+
+    def get_mail(self) -> str:
+        status: html = self.session.get(
+            url     = f'{self.base_url}', 
+            proxies = self.proxies, 
+            timeout = self.timeout
+        ).status_code
+        try:
+            if status == 200:
+                data = self.session.post(
+                    url     = f'{self.base_url}/mailbox', 
+                    proxies = self.proxies, 
+                    timeout = self.timeout
+                ).json()
+                self.session.headers['authorization'] = f'Bearer {data["token"]}'
+                return data["token"], data["mailbox"]
+        except:
+            return 'Email creation error.'
+    
+    def fetch_inbox(self) -> json:
+        response = self.session.get(
+            url = f'{self.base_url}/messages',
+                proxies = self.proxies, 
+                timeout = self.timeout
+        ).json()
+        return response
+    
+    def get_message_content(self, message_id: str):
+        response = self.session.get(
+            url = f'{self.base_url}/messages/{message_id}',
+            proxies = self.proxies, 
+            timeout = self.timeout
+        ).json()
+        return response["bodyHtml"]
+
+email_client = TempMail()
+token, email = email_client.get_mail()
+print(email)
+print(token)
+
+while True:
+    time.sleep(5)
+    messages = email_client.fetch_inbox()
+    print(messages)
+    
+    if len(messages["messages"]) > 0:
+
+        email_content = email_client.get_message_content(messages["messages"][0]["_id"])
+        print(email_content)
